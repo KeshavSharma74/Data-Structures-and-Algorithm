6664cca5a86d01e5ef6b1fce0a1d5fc58adc480d
keshavsharma74 <keshav165p@gmail.com>
2024-09-01 10:46:19 +0530

    Add Doubly Linked List

---
+++ LinkedList/DoublyLinkedList.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* prev;
+        Node* next;
+        Node(int data){
+            this->data=data;
+            this->next=NULL;
+            this->prev=NULL;
+        }
+};
+
+int main(){
+    
+    return 0;
+}
