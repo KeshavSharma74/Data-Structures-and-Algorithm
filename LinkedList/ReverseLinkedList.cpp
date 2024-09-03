b33424fdd743096a08f782a8c10fd15a7542ebd2
keshavsharma74 <keshav165p@gmail.com>
2024-09-03 22:03:37 +0530

    Add Reverse Linked List

---
+++ LinkedList/ReverseLinkedList.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node(int data){
+            this->data=data;
+            this->next=NULL;
+        }
+};
+
+void print(Node* head){
+    Node* temp=head;
+    for(;head;head=head->next)  cout<<head->data<<"->";
+    cout<<"NULL"<<endl;
+}
+
+void insertAtTail(Node* &tail,int data){
+    Node* temp=new Node(data);
+    tail->next=temp;
+    tail=temp;
+}
+
+Node* reverseLinkedListUsingLoop(Node* head){
+    Node* prev=NULL;
+    Node* forward=NULL;
+    Node* curr=head;
+    while(curr!=NULL){
+        Node* forward=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=forward;
+    }
+    return prev;
+}
+
+int main(){
+    Node* head=new Node(10);
+    Node* tail=head;
+    insertAtTail(tail,11);
+    insertAtTail(tail,12);
+    insertAtTail(tail,13);
+    insertAtTail(tail,14);
+    insertAtTail(tail,15);
+    print(head);
+    head=reverseLinkedListUsingLoop(head);
+    print(head);
+    return 0;
+}
