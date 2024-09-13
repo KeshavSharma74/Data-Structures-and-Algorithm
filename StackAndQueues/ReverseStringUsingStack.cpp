6c01de4f4d2d13959ce1315289893d41010050af
keshavsharma74 <keshav165p@gmail.com>
2024-09-13 23:49:12 +0530

    Edit Code

--- StackAndQueues/ReverseStringUsingStack.cpp
+++ StackAndQueues/ReverseStringUsingStack.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+string reverseString(string str){
+    string ans;
+    stack<int> st;
+    for(int i=0;i<str.size();i++){
+        st.push(str[i]);
+    }
+    while(!st.empty()){
+        ans+=st.top();
+        st.pop();
+    }
+    return ans;
+}
+
 int main(){
     string str="keshav sharma";
     string ans;
