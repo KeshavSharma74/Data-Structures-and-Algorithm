bde852c2d32368f112d8028adb39e3e030745a6c
keshavsharma74 <keshav165p@gmail.com>
2024-09-19 21:31:40 +0530

    Two Sum Problem

---
+++ Arrays/twoSum.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPresent(vector<int> arr,int target){
+    sort(arr.begin(),arr.end());
+    int left=0,right=arr.size()-1;
+    while(left<right){
+        int sum=arr[left]+arr[right];
+        if(sum==target){
+            return true;
+        }
+        else if(sum<target){
+            left++;
+        }
+        else{
+            right--;
+        }
+    }
+    return false;
+}
+
+int main(){
+    vector<int> arr={1,3,4,3,5,3,5};
+    int target=10;
+    cout<<isPresent(arr,10);
+    return 0;
+}
