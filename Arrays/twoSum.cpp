3f2ad1ad77669a31da157bbb8ecbb6e036a0cbfe
keshavsharma74 <keshav165p@gmail.com>
2024-09-22 01:07:54 +0530

    Add Two Sum Problem

--- Arrays/twoSum.cpp
+++ Arrays/twoSum.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// this can also be done using map
+
 bool isPresent(vector<int> arr,int target){
     sort(arr.begin(),arr.end());
     int left=0,right=arr.size()-1;
