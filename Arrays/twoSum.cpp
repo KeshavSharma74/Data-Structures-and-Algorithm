5747e13192a3bc328c59f81ebeffacdc26c08f84
keshavsharma74 <keshav165p@gmail.com>
2024-09-26 19:35:33 +0530

    Two Sum Problem

--- Arrays/twoSum.cpp
+++ Arrays/twoSum.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<int> twoSum(vector<int> arr,int target){
+    unordered_map<int,int> m;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int moreNeeded=target-arr[i];
+        if(m.find(moreNeeded)!=m.end()){
+            return {i,m[moreNeeded]};
+        }
+        m[arr[i]]=i;
+    }
+    return {-1,-1};
+}
+
 // this can also be done using map
 
 bool isPresent(vector<int> arr,int target){
