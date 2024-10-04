86e170d0b7d6b3a511b57a50dcdefbcfe0418fb6
keshavsharma74 <keshav165p@gmail.com>
2024-10-04 23:32:33 +0530

    Double Reversal Number

---
+++ Arrays/cofr5.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isSameAfterReversals(int num) {
+        return (num==0 || num%10!=0);
+    }
+};
+
+int main(){
+    
+    return 0;
+}
